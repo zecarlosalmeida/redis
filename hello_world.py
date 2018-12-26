25287bafad23ed0d07670b017b85d9f2cc6574fc
zecarlosalmeida <zekarlos@gmail.com>
2018-12-26 17:41:45 -0200

    3

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,20 @@
-print("This line will be printed.")
+import redis
+
+myHostname = "<Your Host Name>.redis.cache.windows.net"
+myPassword = "<Your Access Key>"
+
+r = redis.StrictRedis(host=myHostname, port=6380,password=myPassword,ssl=True)
+
+result = r.ping()
+print("Ping returned : " + str(result))
+
+result = r.set("Message", "Hello!, The cache is working with Python!")
+print("SET Message returned : " + str(result))
+
+result = r.get("Message")
+print("GET Message returned : " + result.decode("utf-8"))
+
+result = r.client_list()
+print("CLIENT LIST returned : ") 
+for c in result:
+    print("id : " + c['id'] + ", addr : " + c['addr'])
